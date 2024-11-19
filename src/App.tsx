8f58cc0b033552094ae1d9eca853d0d1c498692b
Hamid Faraji <h.faraji@zi-tel.com>
2024-11-19 22:39:13 +0330

    Configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <p className="read-the-docs">
-      Hello World
-    </p>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
